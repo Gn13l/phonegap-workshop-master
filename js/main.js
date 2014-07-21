b1bbee8a17906cc00d1f4738b9f1e21791c8c3f1
Daniel Grimes <daniel.w.grimes@gmail.com>
2014-07-21 09:22:24 -0500

    Changed to single page architecture.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var app = {
 
+    renderHomeView: function() {
+        var html = 
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key' type='text'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>";
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
     showAlert: function (message, title) {
         if(navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -24,7 +35,8 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function () {
-            self.showAlert('Store Initialized', 'Info');
+            self.showAlert('About to render home.', 'Info');
+            self.renderHomeView();
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
